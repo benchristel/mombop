367dba21ab7e6fc0b18535e241bce382032b19c4
Ben Christel <ben.christel@gmail.com>
2024-12-25 11:13:31 -0700

    Add Jethro Tull

--- src/data/channel2.ts
+++ src/data/channel2.ts
@@ -35,6 +35,85 @@ export const episodes = [
     bitMdn_PbOg 18:24 The Moody Blues - Days Of Future Passed - 1967 Vinyl LP - Side 1
     jktBW1Etu3M 22:42 The Moody Blues - Days Of Future Passed - 1967 Vinyl LP - Side 2
     `,
+    `
+    # Jethro Tull - Thick As A Brick
+    ldXdnZtTWp8 22:40 Thick as a Brick (Pt. 1) (1997 Remaster)
+    GTWQv8RsI6s 21:10 Thick as a Brick (Pt. 2) (1997 Remaster)
+    `,
+    `
+    # Jethro Tull - Songs From The Wood
+    z4UYX2qpUK0 4:54 Songs from the Wood (2003 Remaster)
+    9fWzUgrYnqs 2:29 Jack-in-the-Green (2003 Remaster)
+    KNc2uifQuGw 4:32 Cup of Wonder (2003 Remaster)
+    royymUiwItI 5:12 Hunting Girl (2003 Remaster)
+    XJS9TjjHxx8 3:44 Ring Out, Solstice Bells (2003 Remaster)
+    # 6-ANplhDJNY 6:03 Velvet Green (2003 Remaster)
+    2mBaE-K4RxI 3:32 The Whistler (2003 Remaster)
+    # pkUCchG0Kls 8:34 Pibroch (Cap in Hand) (2003 Remaster)
+    73LAuGkgayo 2:28 Fire at Midnight (2003 Remaster)
+    XzJVZ5Uda84 5:19 Beltane (2003 Remaster)
+    `,
+    `
+    # Jethro Tull - Benefit
+    gfKzPV-Ely4 6:19 With You There to Help Me (2001 Remaster)
+    b1Pzk_UYnos 5:15 Nothing to Say (2001 Remaster)
+    2gfeRCSNBQM 2:49 Alive and Well and Living In (2001 Remaster)
+    KQpsbE6pH8s 2:52 Son (2001 Remaster)
+    eG5zRt-sNWE 3:52 For Michael Collins, Jeffrey and Me (2001 Remaster)
+    S5vto70Q23E 6:14 To Cry You a Song (2001 Remaster)
+    c5OjM3G6vnQ 2:45 A Time for Everything (2001 Remaster)
+    j1VYRZF8bCs 3:50 Inside (2001 Remaster)
+    Wdv8PmIDLCQ 3:50 Play in Time (2001 Remaster)
+    mp5dXgf9MwA 4:43 Sossity; You're a Woman (2001 Remaster)
+    g6iyWNTnVRI 3:08 Singing All Day (2001 Remaster)
+    UhbRLQmzrD4 3:52 The Witch's Promise (2001 Remaster)
+    qizGFSKWAvA 1:38 Just Trying to Be (2001 Remaster)
+    xkzeeEU27Mw 3:48 Teacher (Single Mix) (2001 Remaster)
+    `,
+    `
+    # Jethro Tull - Best of Acoustic
+    GW-TrUDrv1E 2:51 Fat Man (2001 Remaster)
+    Kq2TIzIOJ-4 3:18 Life Is a Long Song (2001 Remaster)
+    DoPFe76HRJw 1:21 Cheap Day Return
+    1VvNSJwK3GI 3:53 Mother Goose
+    IG63iywGiJU 1:54 Wond'ring Aloud
+    TbKBz2AUGJI 3:03 Thick as a Brick (Edit #1) (2001 Remaster)
+    1oyMmz4FiAQ 4:12 Skating Away On the Thin Ice of the New Day (2002 Remaster)
+    TQHHhvCw0Kk 1:30 Cold Wind to Valhalla (Intro) (2002 Remaster)
+    33Op6mza7f8 4:37 One White Duck / 0" = Nothing at All (2002 Remaster)
+    mTvzkXEiDUU 2:52 Salamander (2002 Remaster)
+    qnNokfzPJ7Y 2:29 Jack-in-the-Green (2003 Remaster)
+    Ii4G2lwyXks 6:03 Velvet Green (2003 Remaster)
+    ru1_S2wFuzs 2:43 Dun Ringill (2004 Remaster)
+    sdKsD5e8NgM 3:23 Jack Frost and the Hooded Crow (2007 Remaster)
+    d7Up0aGXhmM 2:14 Under Wraps #2 (2005 Remaster)
+    jQjy2q6uU0M 4:57 Jack-a-Lynn
+    vaJpbwKc4gM 2:01 Some Day the Sun Won't Shine for You
+    G2RqFpju02o 3:39 Broadford Bazaar (2007 Remaster)
+    UCi0wZjyx_Y 2:58 The Water Carrier
+    KZMO87qRRtw 3:01 Rupi's Dance
+    7mOI9xaY_Ac 2:50 A Christmas Song
+    e1ga16WVCO0 4:03 Weathercock (2003 Remaster)
+    krQyHXul-o8 3:41 One Brown Mouse (2007 Remaster)
+    zwf2svLUi1Q 4:14 Pastime with Good Company (Live in Denmark)
+    `,
+    `
+    # Jethro Tull - Stand Up
+    JajGBIfGWZM 4:09 A New Day Yesterday (2001 Remaster)
+    wlNwOJkP5P8 2:13 Jeffrey Goes to Leicester Square (2001 Remaster)
+    dnEx_rNOijs 3:47 Bourée (2001 Remaster)
+    a3uqdoJxJ50 3:54 Back to the Family (2001 Remaster)
+    A6VXRSVZHIY 4:24 Look into the Sun (2001 Remaster)
+    Nu_xOl-I7Lc 4:25 Nothing Is Easy (2001 Remaster)
+    iznQpk9GLaA 2:51 Fat Man (2001 Remaster)
+    oGvux7w1Ea4 4:04 We Used to Know (2001 Remaster)
+    exXKfGPgW00 4:08 Reasons for Waiting (2001 Remaster)
+    r-ZDY-NtpII 4:22 For a Thousand Mothers (2001 Remaster)
+    8mSIX8G_0Mk 3:23 Living in the Past (2001 Remaster)
+    7u8Me9lgosc 2:45 Driving Song (2001 Remaster)
+    mfvGAG1N7g8 4:04 Sweet Dream (2001 Remaster)
+    Mxr9R2GvMbk 3:08 17 (2001 Remaster)
+    `,
 ]
 
 export const videos = `
